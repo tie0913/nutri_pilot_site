6aaf39245199cdaf684c417053ba571507845e9b
wangtieforcs <wangtie_913@outlook.com>
2026-04-16 21:17:34 -0600

    remove members who did not do anything

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,7 @@
 <script setup>
 
 import tie from '@/assets/tie.jpg'
-import smit from '@/assets/smit.jpg'
-import nihar from '@/assets/Nihar.jpg'
-import dhruv from '@/assets/dhruv.jpg'
 import alex from '@/assets/alex_icon.png'
-import placeholder from '@/assets/placeholder.jpg'
 import kelci from '@/assets/kelci.png'
 </script>
 
@@ -34,10 +30,9 @@ import kelci from '@/assets/kelci.png'
                     <!-- Engineering -->
                     <div>
                         <h3 class="text-2xl font-semibold text-gray-900 mb-10">
-                            Engineering Team
+                            Engineering
                         </h3>
                         <div class="mt-10 space-y-10">
-
                             <div class="flex items-center gap-4">
                                 <img :src="tie" alt="Tie Wang" class="w-16 h-16 rounded-full object-cover" />
                                 <!-- Tie Wang -->
@@ -52,68 +47,7 @@ import kelci from '@/assets/kelci.png'
                                     </p>
                                 </div>
                             </div>
-
-                            <div class="flex items-center gap-4">
-                                <img :src="dhruv" alt="Dhruv Patel" class="w-16 h-16 rounded-full object-cover" />
-                                <!-- Dhruv Patel -->
-                                <div>
-                                    <a href="https://www.linkedin.com/in/dhruv-patel-b7b7a7212/" target="_blank"
-                                        rel="noopener noreferrer"
-                                        class="text-xl font-semibold text-gray-900 hover:text-green-700 transition-colors duration-200">
-                                        Dhruv Patel
-                                    </a>
-                                    <p class="mt-2 text-sm text-gray-500">
-                                        Tester
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="flex items-center gap-4">
-                                <img :src="nihar" alt="Nihar Parekh" class="w-16 h-16 rounded-full object-cover" />
-                                <!-- Nihar Parekh -->
-                                <div>
-                                    <a href="https://www.linkedin.com/in/nihar-parekh-3088a6239/" target="_blank"
-                                        rel="noopener noreferrer"
-                                        class="text-xl font-semibold text-gray-900 hover:text-green-700 transition-colors duration-200">
-                                        Nihar Parekh
-                                    </a>
-                                    <p class="mt-2 text-sm text-gray-500">
-                                        AI Tester
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class="flex items-center gap-4">
-                                <img :src="smit" alt="Smit Ashwinbhai Parmar"
-                                    class="w-16 h-16 rounded-full object-cover" />
-                                <!-- Smit -->
-                                <div>
-                                    <a href="https://www.linkedin.com/in/smit-parmar-745a89363/" target="_blank"
-                                        rel="noopener noreferrer"
-                                        class="text-xl font-semibold text-gray-900 hover:text-green-700 transition-colors duration-200">
-                                        Smit Ashwinbhai Parmar
-                                    </a>
-                                    <p class="mt-2 text-sm text-gray-500">
-                                        UI Designer
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="flex items-center gap-4">
-                                <!-- Arya Bharatbhai Patel -->
-                                <img :src="placeholder" alt="Ayra Bharatbhai Patel"
-                                    class="w-16 h-16 rounded-full object-cover" />
-                                <div>
-                                    <a href="#" target="_blank" rel="noopener noreferrer"
-                                        class="text-xl font-semibold text-gray-900 hover:text-green-700 transition-colors duration-200">
-                                        Arya Bharatbhai Patel
-                                    </a>
-                                    <p class="mt-2 text-sm text-gray-500">
-                                        UI Designer
-                                    </p>
-                                </div>
-                            </div>
                         </div>
-
-
                     </div>
 
                     <!-- Faculty -->
